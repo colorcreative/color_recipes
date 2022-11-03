9628a6e5e4f50369f1305c423ed2b9d6b6fbabdc
Sam Mank <stuart.mank@gmail.com>
2022-11-02 17:10:37 -0700

    Clean up

--- trust.sh
+++ trust.sh
@@ -1,7 +1,7 @@
 autopkg_repo="/Users/Shared/Repositories/autopkg_recipes/"
 
-ls -R $autopkg_repo | grep munki | while read line
+ls -R $autopkg_repo | grep recipe | while read line
 do
     echo "Creating override for: $line"
-    autopkg make-override $line
+    autopkg make-override --force "$line"
 done
