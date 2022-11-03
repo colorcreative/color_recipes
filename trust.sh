179324d7008b7fafa46c138fabc1d45644f6a343
Sam Mank <stuart.mank@gmail.com>
2022-11-03 11:46:52 -0700

    Cleaned up some recipes

--- trust.sh
+++ trust.sh
@@ -1,7 +1,12 @@
-autopkg_repo="/Users/Shared/Repositories/autopkg_recipes/"
+autopkg_repo="~/Library/Application Support/AutoPkgr/recipe_list.txt"
 
-ls -R $autopkg_repo | grep recipe | while read line
+# ls -R $autopkg_repo | grep recipe | while read line
+cat $autopkg_repo | while read line
 do
     echo "Creating override for: $line"
-    autopkg make-override --force "$line"
+    autopkg make-override $line
+    # echo "Auditing: $line"
+    # autopkg audit "$line"
+    # echo "Updating trust for: $line"
+    # autopkg update-trust-info "$line"
 done
