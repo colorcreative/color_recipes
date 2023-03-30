6d75db2f3b2a2b708ea5ea4763fb45987380668d
Sam Mank <stuart.mank@gmail.com>
2023-03-30 14:33:02 -0700

    Added Duet and DisplayLinkUserAgent recipes

--- trust.sh
+++ trust.sh
@@ -1,13 +1,30 @@
+#!/bin/bash
 
-autopkg_repo='Users/sammank/Library/Application Support/AutoPkgr/test.txt'
-cd $autopkg_repo
-ls -R | grep recipe | while read line
-#cat $autopkg_repo | while read line
-do
-    echo "Creating override for: $line"
-    autopkg make-override "$line" --force
-    # echo "Auditing: $line"
-    # autopkg audit "$line"
-    # echo "Updating trust for: $line"
-    # autopkg update-trust-info "$line"
+autopkg_repo="/Users/sammank/Library/AutoPkg/RecipeRepos"
+cd "$autopkg_repo" || { echo "Error: Failed to change to directory '$autopkg_repo'"; exit 1; }
+
+# Find all recipe files in the current directory and its subdirectories
+recipe_files=$(find . -type f -name "*.recipe")
+
+# Debugging line: Print the list of recipe files found
+echo "Recipe files found:"
+echo "$recipe_files"
+
+# Save the current Internal Field Separator (IFS) value
+oldIFS="$IFS"
+
+# Set IFS to a newline character to properly handle file paths
+IFS=$'\n'
+
+# Iterate over each recipe file and create an override
+for recipe_file in $recipe_files; do
+    # Remove leading './' and file extension to get the recipe name
+    recipe_name="${recipe_file#./}"
+    recipe_name="${recipe_name%.recipe}"
+
+    echo "Creating override for: $recipe_name"
+    autopkg make-override "$recipe_name" --force
 done
+
+# Restore the original IFS value
+IFS="$oldIFS"
